1e2081c9e9756570161cec8466c914ce7bffbacd
balpie <pietrobalestri2004@gmail.com>
2025-11-07 18:47:58 +0100

    Aggiustato layout, aggiunto db

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # tritesWebApp
 Trites, a game that probably will remind you of an other one very similar...
 - Project written for Progettazione Web course @unipi
+
+## Comandi
+- Invio: hard drop (+ 10pts * liv)
+- Right Shift: hold
+- W: ruota
+- A, D: sposta a sinistra, destra
+- S: Scendi veloce
+- Pausa: Ferma il clock del computer
---
+++ trites_db.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS balestri_665384;
+USE balestri_665384;
+
+DROP TABLE IF EXISTS Utenti;
+
+CREATE TABLE IF NOT EXISTS Utenti(
+    `NomeUtente` VARCHAR(30) NOT NULL,
+    `PasswordUtente` VARCHAR(64) NOT NULL, -- Hash with: "PASSWORD"
+    `DataIscrizione` DATE NOT NULL,
+    PRIMARY KEY(`NomeUtente`)
+);
+
+DROP TABLE IF EXISTS Partite;
+
+CREATE TABLE IF NOT EXISTS Partite(
+    `IdPartita` INTEGER NOT NULL AUTO_INCREMENT,
+    `NomeUtente` VARCHAR(30) NOT NULL,
+    `LineeRipulite` INTEGER NOT NULL,
+    `Punti` INTEGER NOT NULL,
+    `DataPartita` DATETIME NOT NULL,
+    `Durata` INTEGER NOT NULL, -- Durata partita in secondi
+    FOREIGN KEY(`NomeUtente`) REFERENCES `Utenti`(`NomeUtente`),
+    PRIMARY KEY(`IdPartita`)
+);
